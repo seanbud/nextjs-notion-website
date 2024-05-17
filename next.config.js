5dc741faeb179b8844ddcd360d713dca89c0bcd1
Sean Budning <seanbud@users.noreply.github.com>
2024-05-17 00:21:27 -0700

    Revert "added basepath to app config"
    This reverts commit 803464fa7be45659b26372e95a6e1a4564760aeb.

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
-  basePath: '/portfolio',
   images: {
     domains: [
       'www.notion.so',
