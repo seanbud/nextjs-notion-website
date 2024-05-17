803464fa7be45659b26372e95a6e1a4564760aeb
Sean Budning <seanbud@users.noreply.github.com>
2024-05-17 00:04:22 -0700

    added basepath to app config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
+  basePath: '/portfolio',
   images: {
     domains: [
       'www.notion.so',
